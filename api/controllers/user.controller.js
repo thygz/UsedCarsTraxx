faa29531321f8462a178f0afdabb258ddb825c68
Teejay Villaluz <traxxjester25@gmail.com>
2023-11-14 12:02:58 +0800

    Create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const testing = (req, res) => {
+    res.json({
+        message: 'Api route is working',
+    });
+};
---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { testing } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.get('/test', testing);
+
+export default router;
