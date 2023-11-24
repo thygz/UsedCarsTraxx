ec38d97cc70133561f89b426010bef3cea88b814
Teejay Villaluz <traxxjester25@gmail.com>
2023-11-24 12:25:24 +0800

    Update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
         avatar: {
             type: String,
             default:
-                'https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg',
+                'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
         },
     },
     { timestamps: true }
