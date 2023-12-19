34f50ac3ed0e4a382ac9a1d8865bfd55eac1f913
Teejay Villaluz <traxxjester25@gmail.com>
2023-12-19 10:56:03 +0800

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,37 @@
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">
+                About Sahand Estate
+            </h1>
+            <p className="mb-4 text-slate-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores distinctio impedit delectus eum cum, laboriosam
+                possimus debitis quas, doloribus, quibusdam pariatur modi atque
+                odio architecto vel ratione! Nulla corrupti saepe excepturi
+                quia! Iure inventore odio commodi sapiente cum enim atque non
+                maiores voluptates vel rem odit nisi mollitia, incidunt
+                consequatur!
+            </p>
+            <p className="mb-4 text-slate-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                incidunt debitis illum officiis nulla numquam beatae perferendis
+                minima adipisci reprehenderit provident illo nam nostrum
+                laborum, corrupti quos delectus? Obcaecati, est sint, sunt et
+                ratione odio maiores beatae sed accusamus vero voluptas odit
+                iste quod ab.
+            </p>
+            <p className="mb-4 text-slate-700">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum
+                temporibus laboriosam libero quae maxime ut sit, tempora quidem
+                aut vel blanditiis reprehenderit culpa autem! Officia possimus
+                nisi blanditiis odit nesciunt sequi, ratione rerum reprehenderit
+                ea repellat illum minus in. Laboriosam voluptatum vel illo
+                ducimus dolorem similique. Sed asperiores deserunt consectetur
+                veritatis fugiat eligendi vitae quam distinctio fugit
+                repellendus. Perspiciatis accusantium iure quasi in sapiente
+                iusto!
+            </p>
+        </div>
+    );
 }
