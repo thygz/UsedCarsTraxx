86e55730c63dcf7d1f19c4bb15e9e336bf4e365e
Teejay Villaluz <traxxjester25@gmail.com>
2024-07-01 17:23:27 +0800

    Add style, responsiveness and note

--- client/src/components/SearchInput.jsx
+++ client/src/components/SearchInput.jsx
@@ -55,44 +55,44 @@ export default function SearchInput() {
     };
 
     return (
-        <div className="max-w-full lg:max-w-4xl mx-auto px-5 min-[500px]:px-20 min-[610px]:px-32 md:px-10 py-10 bg-slate-200 shadow-md lg:rounded-xl flex flex-col gap-8 justify-center items-center mt-[-2.5rem] lg:mt-[-7rem]">
+        <div className="max-w-full lg:max-w-4xl mx-auto px-5 min-[500px]:px-20 min-[610px]:px-32 md:px-10 pt-10 pb-10 md:pb-3 bg-slate-200 shadow-md lg:rounded-xl flex flex-col gap-1 md:gap-7 justify-center items-center mt-[-2.5rem] lg:mt-[-7rem]">
             <form
                 onSubmit={handleSubmit}
-                className="flex flex-col lg:flex-row justify-center items-end gap-5 lg:gap-3 w-full"
+                className="flex flex-col lg:flex-row justify-center items-center lg:items-end gap-8 md:gap-4 lg:gap-3 w-full"
             >
                 <div className="flex flex-1 flex-col md:flex-row gap-3 w-full">
                     <div className="flex flex-col flex-1">
-                        <p className="text-xs font-semibold px-1 text-gray-700 uppercase">
+                        <p className="text-xs font-semibold px-1 text-gray-900 uppercase">
                             Make
                         </p>
                         <input
                             type="text"
                             // placeholder="Search brand, price and more"
-                            className="border border-slate-400 bg-gray-50 px-3 py-2 rounded-md text-base text-gray-600 focus:outline-gray-300"
+                            className="border border-slate-400 bg-white px-3 py-2 rounded-md text-base text-gray-700 focus:outline-gray-400"
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
                         />
                     </div>
                     <div className="flex flex-col flex-1">
-                        <p className="text-xs font-semibold px-1 text-gray-700 uppercase">
+                        <p className="text-xs font-semibold px-1 text-gray-900 uppercase">
                             Model
                         </p>
                         <input
                             type="text"
                             // placeholder="Search brand, price and more"
-                            className="border border-slate-400 bg-gray-50 px-3 py-2 rounded-md text-base text-gray-600 focus:outline-gray-300"
+                            className="border border-slate-400 bg-white px-3 py-2 rounded-md text-base text-gray-700 focus:outline-gray-400"
                             value={modelSearch}
                             onChange={(e) => setModelSearch(e.target.value)}
                         />
                     </div>
                     <div className="flex flex-col flex-1 w-full">
-                        <label className="text-xs font-semibold px-1 text-gray-700 uppercase">
+                        <label className="text-xs font-semibold px-1 text-gray-900 uppercase">
                             Price
                         </label>
-                        <div className="border border-slate-400 px-3 py-2 rounded-md bg-gray-50">
+                        <div className="border border-slate-400 px-3 py-2 rounded-md bg-white">
                             <select
                                 id="price_order"
-                                className="rounded-sm text-sm text-gray-600 focus:outline-none w-full bg-gray-50 cursor-pointer"
+                                className="rounded-sm text-sm text-gray-700 focus:outline-none w-full bg-white cursor-pointer"
                                 onChange={handleChange}
                                 value={`${sidebardata.sort}_${sidebardata.order}`}
                                 // defaultValue={sidebardata.sort}
@@ -113,7 +113,7 @@ export default function SearchInput() {
                         </div>
                     </div>
                 </div>
-                <div className="w-full lg:w-auto">
+                <div className="w-full md:w-auto flex justify-center items-center">
                     {/* <div className="flex flex-col flex-1 w-full">
                         <label className="text-xs font-semibold px-1 text-gray-700 uppercase">
                             Price
@@ -141,11 +141,17 @@ export default function SearchInput() {
                             </select>
                         </div>
                     </div> */}
-                    <button className="bg-cyan-800 text-white text-sm sm:text-base font-semibold rounded-sm capitalize hover:opacity-90 px-6 py-[0.52rem] flex justify-center items-center w-full">
+                    <button className="bg-cyan-800 text-white text-sm sm:text-base font-semibold rounded-md capitalize hover:opacity-90 px-20 lg:px-6 py-3 sm:py-[0.6rem] lg:py-[0.52rem] flex justify-center items-center w-full">
                         Search
                     </button>
                 </div>
             </form>
+            <div className="w-full">
+                <p className="text-[0.7rem] text-center text-slate-500">
+                    Note: Can search even one input is field or key word is not
+                    complete.
+                </p>
+            </div>
         </div>
     );
 }
