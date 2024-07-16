1d190c1c7c457e5000136836b9f5a99c56d2ba01
Teejay Villaluz <traxxjester25@gmail.com>
2024-07-16 09:43:14 +0800

    Add react toastify and some styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css';
 import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <App />
+            <ToastContainer />
         </PersistGate>
     </Provider>
 );
