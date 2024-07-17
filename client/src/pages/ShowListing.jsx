93a6f570412e6ea7c431375011d98646fd9b978d
Teejay Villaluz <traxxjester25@gmail.com>
2024-07-17 12:17:00 +0800

    Add confirm delete modal and toast notification

--- client/src/pages/ShowListing.jsx
+++ client/src/pages/ShowListing.jsx
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { MdOutlineEdit } from 'react-icons/md';
 import ScrollToTop from '../components/ScrollToTop';
+import { toast } from 'react-toastify';
 
 export default function ShowListing() {
     const { currentUser, loading, error } = useSelector((state) => state.user);
     const [showListingsError, setShowListingsError] = useState(false);
     const [listingLoading, setListingLoading] = useState(false);
-    const [confirmDelete, setConfirmDelete] = useState(false);
+    const [toggleDeleteModal, setToggleDeleteModal] = useState(false);
+    const [deleteId, setDeleteId] = useState('');
+    const [deleteMake, setDeleteMake] = useState('');
+    const [deleteModel, setDeleteModel] = useState('');
     const [userListings, setUserListings] = useState([]);
 
     useEffect(() => {
@@ -49,18 +53,32 @@ export default function ShowListing() {
             setUserListings((prev) =>
                 prev.filter((listing) => listing._id !== listingId)
             );
+            toast.info('Vehicle unsaved', {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+            });
         } catch (error) {
             console.log(error.message);
         }
+    };
+
+    const handleOpenDeleteModal = (listingId, make, model) => {
+        setDeleteId(listingId);
+        setDeleteMake(make);
+        setDeleteModel(model);
+        setToggleDeleteModal(true);
     };
 
     return (
         <div className="px-3 pt-10 pb-14 max-w-3xl mx-auto min-h-[75vh]">
             <ScrollToTop />
             <div className="p-3 bg-inherit rounded-sm">
-                {/* <p className="text-red-700 mt-5 text-center font-semibold text-2xl">
-                    {showListingsError ? 'Error showing listings' : ''}
-                </p> */}
                 {userListings && userListings.length > 0 ? (
                     <div className="flex flex-col gap-4">
                         <h1 className="text-2xl font-semibold text-slate-800 text-center mt-5 mb-7">
@@ -91,7 +109,11 @@ export default function ShowListing() {
                                 <div className="flex gap-3 items-center">
                                     <button
                                         onClick={() =>
-                                            handleDeleteListing(listing._id)
+                                            handleOpenDeleteModal(
+                                                listing._id,
+                                                listing.make,
+                                                listing.model
+                                            )
                                         }
                                         className="text-lg bg-slate-200 p-1 hover:bg-slate-300 duration-300 rounded-sm"
                                     >
@@ -102,6 +124,35 @@ export default function ShowListing() {
                                             <MdOutlineEdit />
                                         </button>
                                     </Link>
+                                </div>
+                                <div
+                                    className={`fixed flex flex-col justify-center items-center gap-5 inset-0 w-80 sm:w-96 h-32 rounded-lg mx-auto my-auto bg-slate-50 p-3 sm:p-5 z-50 ${
+                                        toggleDeleteModal ? 'block' : 'hidden'
+                                    }`}
+                                >
+                                    <p className="text-center text-sm text-slate-800 font-semibold">
+                                        Are you sure you want to delete{' '}
+                                        {deleteMake} {deleteModel}?
+                                    </p>
+                                    <div className="flex gap-5">
+                                        <button
+                                            className="text-white text-sm bg-red-600 w-24 h-8 rounded-md hover:bg-opacity-90 font-medium"
+                                            onClick={() => {
+                                                handleDeleteListing(deleteId);
+                                                setToggleDeleteModal(false);
+                                            }}
+                                        >
+                                            Delete
+                                        </button>
+                                        <button
+                                            className="text-sm bg-white w-24 h-8 rounded-md shadow-sm font-medium"
+                                            onClick={() =>
+                                                setToggleDeleteModal(false)
+                                            }
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         ))}
@@ -124,6 +175,19 @@ export default function ShowListing() {
                         )}
                     </div>
                 )}
+                <div className="hidden">
+                    {toggleDeleteModal
+                        ? (document.body.style.overflow = 'hidden')
+                        : (document.body.style.overflow = 'auto')}
+                </div>
+                <div
+                    onClick={() => setToggleDeleteModal(false)}
+                    className={`fixed bg-black bg-opacity-70 z-40 top-0 left-0 right-0 bottom-0 ${
+                        toggleDeleteModal
+                            ? 'opacity-1 pointer-events-auto'
+                            : 'opacity-0 pointer-events-none'
+                    }`}
+                ></div>
             </div>
         </div>
     );
